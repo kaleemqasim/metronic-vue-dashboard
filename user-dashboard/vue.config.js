9b3c3a346aceb543cf77cf335f9d14cc67b9090d
Kaleem Qasim <kaleemullahlqp@gmail.com>
2023-04-20 23:11:53 +0500

    path fixes for assets

--- user-dashboard/vue.config.js
+++ user-dashboard/vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/metronic8/vue/demo1/" : "/",
+  publicPath: ''
+
+  // publicPath:
+  //   process.env.NODE_ENV === "production" ? "/metronic8/vue/demo1/" : "/",
 };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/metronic8/vue/demo1/" : "/",
+  publicPath: ''
+  // publicPath:
+  //   process.env.NODE_ENV === "production" ? "/metronic-vue-dashboard/" : "/",
 };
